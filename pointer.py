ff7f19c46c4620ee2e895212af942a03b54e611e
Miro Macko <miro.macko@nozdormu.net>
2022-04-23 19:55:03 +0200

    remove shit

--- pointer.py
+++ pointer.py
@@ -2,16 +2,9 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 import shapefile
 
-point = Point(0.5, 0.5)
-polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
-print(polygon.contains(point))
-
 polygons = []
 sf = shapefile.Reader("slope_deformations_reference_polygons/reference_data_existing_landslides_polygons.shp")
 shapes = sf.shapes()
-
-shape = shapes[25]
-print(shape.points)
 
 for shape in shapes:
     polygons.append(Polygon(shape.points))
