c32601b9a39e71b99b3ad1fb9808bb5de5504eb0
Miro Macko <miro.macko@nozdormu.net>
2022-04-23 20:33:41 +0200

    optimize is in

--- pointer.py
+++ pointer.py
@@ -5,9 +5,22 @@ import shapefile
 polygons = []
 sf = shapefile.Reader("slope_deformations_reference_polygons/reference_data_existing_landslides_polygons.shp")
 shapes = sf.shapes()
+rectangles = []
 
 for shape in shapes:
     polygons.append(Polygon(shape.points))
+    rectangles.append(shape.bbox)
+
+def is_in_optimised(x, y):
+    for i in range(len(polygons)):
+        rectangle = rectangles[i]
+        bottom_left_x, bottom_left_y, upper_right_x, upper_right_y = rectangle[0], rectangle[1], rectangle[2], rectangle[3]
+        if x >= bottom_left_x and x <= upper_right_x and y >= bottom_left_y and y <= upper_right_y:
+            polygon = polygons[i]
+            if polygon.contains(Point(x, y)):
+                return True
+
+    return False
 
 def is_in_polygon(x, y):
     point = Point(x, y)
